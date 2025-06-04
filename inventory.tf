ee21833a9f251d92151b1583f602e2ab41313fb3
Alan Lee <alanleunglee@gmail.com>
2025-06-04 09:58:29 -0700

    refactor: inventory

--- inventory.tf
+++ inventory.tf
@@ -1,11 +1,39 @@
 resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.yml.tpl", {
-    controlplane_nodes = {
-      for k, v in proxmox_virtual_environment_container.k8s_master : k => v.ipv4_addresses[0]
-    }
-    worker_nodes = {
-      for k, v in proxmox_virtual_environment_vm.k8s_worker : k => v.ipv4_addresses[1][0]
+  content = yamlencode({
+    all = {
+      children = {
+        controlplane = {
+          hosts = {
+            for k, v in proxmox_virtual_environment_container.k8s_master : k => {
+              ansible_host                 = length(v.ipv4_addresses) > 0 ? v.ipv4_addresses[0] : "pending"
+              ansible_user                 = "root"
+              ansible_ssh_private_key_file = "~/.ssh/id_rsa"
+            }
+          }
+        }
+        workers = {
+          hosts = {
+            for k, v in proxmox_virtual_environment_vm.k8s_worker : k => {
+              ansible_host                 = length(v.ipv4_addresses) > 1 ? v.ipv4_addresses[1][0] : "pending"
+              ansible_user                 = "debian"
+              ansible_ssh_private_key_file = "~/.ssh/id_rsa"
+              ansible_become               = true
+            }
+          }
+        }
+        k8s_cluster = {
+          children = {
+            controlplane = {}
+            workers      = {}
+          }
+        }
+      }
     }
   })
   filename = "./inventory.yml"
+
+  depends_on = [
+    proxmox_virtual_environment_container.k8s_master,
+    proxmox_virtual_environment_vm.k8s_worker
+  ]
 }
