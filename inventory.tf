a65e1645916e93251b8deccdf7662d0461fc3133
Alan Lee <alanleunglee@gmail.com>
2025-06-04 09:41:09 -0700

    refactor(provider): bpg changes

--- inventory.tf
+++ inventory.tf
@@ -1,7 +1,11 @@
 resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.tpl", {
-    master_ips = { for k, v in proxmox_lxc.k8s_master : k => v.network[0].ip }
-    worker_ips = { for k, v in proxmox_vm_qemu.k8s_worker : k => v.default_ipv4_address }
+  content = templatefile("${path.module}/inventory.yml.tpl", {
+    controlplane_nodes = {
+      for k, v in proxmox_virtual_environment_container.k8s_master : k => v.ipv4_addresses[0]
+    }
+    worker_nodes = {
+      for k, v in proxmox_virtual_environment_vm.k8s_worker : k => v.ipv4_addresses[1][0]
+    }
   })
   filename = "${path.module}/inventory.yml"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
 output "master_ips" {
-  value = { for k, v in proxmox_lxc.k8s_master : k => v.network[0].ip }
+  value = {
+    for k, v in proxmox_virtual_environment_container.k8s_master : k => v.ipv4_addresses[0]
+  }
 }
 
 output "worker_ips" {
-  value = { for k, v in proxmox_vm_qemu.k8s_worker : k => v.default_ipv4_address }
+  value = {
+    for k, v in proxmox_virtual_environment_vm.k8s_worker : k => v.ipv4_addresses[1][0]
+  }
 }
