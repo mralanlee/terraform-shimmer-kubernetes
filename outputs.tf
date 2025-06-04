4d815c79f4422a5d0be63a09824c2fa787ffb6a3
Alan Lee <alanleunglee@gmail.com>
2025-06-04 10:04:39 -0700

    fix: modify ip syntax

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "master_ips" {
   value = {
-    for k, v in proxmox_virtual_environment_container.k8s_master : k => v.ipv4_addresses[0]
+    for k, v in proxmox_virtual_environment_container.k8s_master : k => length(v.ipv4_addresses) > 0 ? v.ipv4_addresses[0] : "pending"
   }
 }
 
 output "worker_ips" {
   value = {
-    for k, v in proxmox_virtual_environment_vm.k8s_worker : k => v.ipv4_addresses[1][0]
+    for k, v in proxmox_virtual_environment_vm.k8s_worker : k => length(v.ipv4_addresses) > 1 ? v.ipv4_addresses[1][0] : "pending"
   }
 }
