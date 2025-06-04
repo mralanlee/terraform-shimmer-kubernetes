e5d70ec55008379b779d8c36bade0058f7ab6d98
Alan Lee <alanleunglee@gmail.com>
2025-06-04 10:09:41 -0700

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,10 @@
-output "controlplane_ips" {
+# outputs.tf - Fixed for bpg provider limitations
+# Note: LXC containers don't expose IP addresses in bpg provider yet
+output "master_ips" {
   value = {
-    for k, v in proxmox_virtual_environment_container.k8s_master : k => length(v.ipv4_addresses) > 0 ? v.ipv4_addresses[0] : "pending"
+    for k, v in proxmox_virtual_environment_container.k8s_master : k => "check-proxmox-gui"
   }
+  description = "Control plane IPs - check Proxmox GUI as bpg provider doesn't expose LXC IPs yet"
 }
 
 output "worker_ips" {
